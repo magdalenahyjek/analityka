8ea805ff091c7722dfd5a2cd319a59311d084619
Magdalena Hyjek <magdalena.lisowska44@gmail.com>
2025-11-14 21:38:14 +0100

    Add files via upload

--- data/shopping_beh.py
+++ data/shopping_beh.py
@@ -26,7 +26,7 @@ def load_data():
     return pd.read_csv(DATA_PATH, encoding="utf-8") #data 
 #%%
 #%%
-col1, col2, col3, col8, col9, col10 = st.columns(6)
+col1, col2, col3, col8, col9 = st.columns(5)
 with col1:
     st.subheader("Data summary")
     st.write(df.describe()) 
@@ -43,10 +43,6 @@ with col8:
     unique_counts = pd.DataFrame(df.nunique(), columns=["Unique Values"])
     st.write(unique_counts)
 with col9:
-    st.subheader("Data shape")
-    rows, cols = df.shape
-    st.write((f"{rows} wierszy x {cols} kolumn"))
-with col10:
     st.subheader("Columns preview")
     st.write(df.head())
 #%%
@@ -80,14 +76,14 @@ col4, col5 = st.columns(2)
 with col4:
     st.subheader("Age x Gender")
     fig = px.histogram(view, x="Age", color="Gender", barmode= "overlay", nbins=30, labels={"Age":"Wiek", "Gender": "Płeć"}, title="Age by Gender", hover_data=["Age"])
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig)
 with col5:
     st.subheader("Purchase Status")
     purchase_counts = view['Item Purchased'].value_counts()
     fig2 = px.bar(y=purchase_counts.values, x=purchase_counts.index, title="Purchase Status Distribution", labels={"names":"Purchase Status", "values":"Count"})
     fig2.update_layout(xaxis_title="Item Purchased", yaxis_title="Amount of Purchase")
 
-    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig2)
 
 
 #%%
@@ -95,7 +91,7 @@ col6, = st.columns(1)
 with col6:
     table_color_gender= view.groupby(["Gender","Color"]).size().reset_index(name="count")
     fig3 = px.bar(table_color_gender, x="Color", y="count", color="Gender", barmode="group", labels={"Gender":"Płeć", "count":"Liczba", "Color":"Color"},title="Gender x Color")
-st.plotly_chart(fig3, use_container_width=True)
+st.plotly_chart(fig3)
 
 
 # %%
@@ -103,7 +99,7 @@ col7, = st.columns(1)
 with col7:
     table_subscription= view.groupby(["Subscription Status","Frequency of Purchases"]).size().reset_index(name="count")
     fig4 = px.bar(table_subscription, x="Subscription Status", y="count", color="Frequency of Purchases", barmode="group", labels={"Subscription":"Subskrypcja", "Frequency of Purchases":"Frequency of Purchases"},title="Subscription Status x Frequency of Purchases")
-st.plotly_chart(fig4, use_container_width=True)
+st.plotly_chart(fig4)
 
 #%%
 #st.write(list(view.columns))
@@ -115,55 +111,70 @@ if "Purchase Amount (USD)" in view.columns:
 # %%
 import plotly.express as px
 
-st.subheader("Top N najczęściej kupowanych produktów")
-n = st.number_input("N (produkty)", min_value=5, max_value=50, value=10, step=1, key="top_items_n")
+#st.subheader("Top N najczęściej kupowanych produktów")
+#n = st.number_input("N (produkty)", min_value=5, max_value=50, value=10, step=1, key="top_items_n")
 
+#if "Item Purchased" in view.columns:
+#    items = (view["Item Purchased"].dropna().value_counts().reset_index())
+#    items.columns = ["Item Purchased", "Count"]
+#    top_items = items.head(int(n))
+#
+#    st.dataframe(top_items)
+#    fig = px.bar(top_items, x="Item Purchased", y="Count", title="Top produkty (liczba sztuk)")
+#    fig.update_layout(xaxis=dict(categoryorder="total descending"))
+#    fig.update_xaxes(tickangle=-45)
+#    st.plotly_chart(fig)
+#else:
+#    st.info("Brak kolumny 'Item Purchased'.")
+#%%
+st.subheader("Top 10 najczęściej kupowanych produktów")
+
+N_TOP = 10  # ustaw tu, jeśli chcesz inny
 if "Item Purchased" in view.columns:
-    items = (view["Item Purchased"].dropna().value_counts().reset_index())
+    items = view["Item Purchased"].dropna().value_counts().reset_index()
     items.columns = ["Item Purchased", "Count"]
-    top_items = items.head(int(n))
+    top_items = items.head(N_TOP)
 
-    st.dataframe(top_items)
-    fig = px.bar(top_items, x="Item Purchased", y="Count", title="Top produkty (liczba sztuk)")
-    fig.update_layout(xaxis=dict(categoryorder="total descending"))
-    fig.update_xaxes(tickangle=-45)
-    st.plotly_chart(fig, use_container_width=True)
+    st.dataframe(top_items, use_container_width=True, hide_index=True)
 else:
     st.info("Brak kolumny 'Item Purchased'.")
 
-# %%
-st.subheader("Średnia kwota zakupu wg lokalizacji")
-
-if {"Location", "Purchase Amount (USD)"} <= set(view.columns):
-    avg_loc = (view.dropna(subset=["Purchase Amount (USD)", "Location"])
-                  .groupby("Location", as_index=False)["Purchase Amount (USD)"].mean()
-                  .rename(columns={"Purchase Amount (USD)": "Avg amount (USD)"})
-                  .sort_values("Avg amount (USD)", ascending=False))
-
-    st.dataframe(avg_loc)
-    fig = px.bar(avg_loc, x="Location", y="Avg amount (USD)", title="Średnia kwota (USD) wg lokalizacji")
-    fig.update_xaxes(tickangle=-45)
-    st.plotly_chart(fig, use_container_width=True)
-else:
-    st.info("Brak kolumn 'Location' i/lub 'Purchase Amount (USD)'.")
 
 # %%
-st.subheader("Top N lokalizacji po wartości sprzedaży (USD)")
-n_loc = st.number_input("N (lokalizacje)", min_value=5, max_value=50, value=10, step=1, key="top_loc_n")
+#st.subheader("Średnia kwota zakupu wg lokalizacji")
 
-if {"Location", "Purchase Amount (USD)"} <= set(view.columns):
-    sales_by_loc = (view.dropna(subset=["Purchase Amount (USD)", "Location"])
-                       .groupby("Location", as_index=False)["Purchase Amount (USD)"].sum()
-                       .rename(columns={"Purchase Amount (USD)": "Sales (USD)"})
-                       .sort_values("Sales (USD)", ascending=False))
+#if {"Location", "Purchase Amount (USD)"} <= set(view.columns):
+#    avg_loc = (view.dropna(subset=["Purchase Amount (USD)", "Location"])
+#                  .groupby("Location", as_index=False)["Purchase Amount (USD)"].mean()
+#                  .rename(columns={"Purchase Amount (USD)": "Avg amount (USD)"})
+#                  .sort_values("Avg amount (USD)", ascending=False))
+#
+#    st.dataframe(avg_loc)
+#    fig = px.bar(avg_loc, x="Location", y="Avg amount (USD)", title="Średnia kwota (USD) wg lokalizacji")
+#    fig.update_xaxes(tickangle=-45)
+#    st.plotly_chart(fig)
+#else:
+#    st.info("Brak kolumn 'Location' i/lub 'Purchase Amount (USD)'.")
 
-    top_loc = sales_by_loc.head(int(n_loc))
+# %%
+#st.subheader("Top N lokalizacji po wartości sprzedaży (USD)")
+#n_loc = st.number_input("N (lokalizacje)", min_value=5, max_value=50, value=10, step=1, key="top_loc_n")
+#
+#if {"Location", "Purchase Amount (USD)"} <= set(view.columns):
+#    sales_by_loc = (view.dropna(subset=["Purchase Amount (USD)", "Location"])
+#                       .groupby("Location", as_index=False)["Purchase Amount (USD)"].sum()
+#                       .rename(columns={"Purchase Amount (USD)": "Sales (USD)"})
+#                       .sort_values("Sales (USD)", ascending=False))
+#
+#    top_loc = sales_by_loc.head(int(n_loc))
+#
+#    st.dataframe(top_loc)
+#    fig2 = px.bar(top_loc, x="Location", y="Sales (USD)", title="Top lokalizacje wg wartości sprzedaży")
+#    fig2.update_xaxes(tickangle=-45)
+#    st.plotly_chart(fig2)
+#else:
+#    st.info("Brak kolumn 'Location' i/lub 'Purchase Amount (USD)'.")
 
-    st.dataframe(top_loc)
-    fig2 = px.bar(top_loc, x="Location", y="Sales (USD)", title="Top lokalizacje wg wartości sprzedaży")
-    fig2.update_xaxes(tickangle=-45)
-    st.plotly_chart(fig2, use_container_width=True)
-else:
-    st.info("Brak kolumn 'Location' i/lub 'Purchase Amount (USD)'.")
+
 
 # %%
